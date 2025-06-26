a6710480056af165bb3fd5294d3c7e74c67ee1af
yogesh priyadarshi <yogeshpriyadarshi55@gmail.com>
2025-06-26 13:08:20 +0530

    pojects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,31 +3,93 @@ import React from "react";
 export default function Projects() {
   return (
     <>
-      <div className="flex flex-wrap">
-        <div className="h-50 w-100 m-5 relative ">
+    <h1 className=" text-4xl text-center text-white m-5"> My <span className="text-red-500 "> Projects</span></h1>
+
+
+
+      <div className="grid grid-cols-3 gap-5 place-items-center">
+
+        <div className="relative group w-75 h-50 overflow-hidden rounded-lg">
           <img
             src="assets\Projects\Tinder.jpeg"
-            alt="Dev-Tinder"
-            className=" h-50 w-100 object-center absolute rounded-4xl hover:scale-105 hover:brightness-125 transition duration-300 cursor-pointer"
+            className="w-full h-full object-cover transition duration-300 group-hover:scale-110"
           />
+          <div
+            className="absolute left-0 right-0 bottom-0  transform translate-y-full group-hover:translate-y-0  
+          flex items-center justify-center bg-gradient-to-t from-blue-500/50 to-white/25 h-full 
+            transition duration-300 text-white text-xl font-semibold"
+          >
+            Hover Text Here
+          </div>
         </div>
-        
-        <div className="relative group w-100 h-50 overflow-hidden rounded-lg">
+ <div className="relative group w-75 h-50 overflow-hidden rounded-lg">
           <img
             src="assets\Projects\Tinder.jpeg"
-            className="w-full h-full object-cover transition duration-300 group-hover:blur-sm"
+            className="w-full h-full object-cover transition duration-300 group-hover:scale-110"
           />
-          <div className="absolute left-0 right-0 bottom-0  transform translate-y-full group-hover:translate-y-0  
-          flex items-center justify-center bg-blue-100 bg-opacity-0 opacity-0 group-hover:opacity-100 transition duration-300 text-white text-xl font-semibold">
+          <div
+            className="absolute left-0 right-0 bottom-0  transform translate-y-full group-hover:translate-y-0  
+          flex items-center justify-center bg-gradient-to-t from-blue-500/50 to-white/25 h-full 
+            transition duration-300 text-white text-xl font-semibold"
+          >
             Hover Text Here
           </div>
         </div>
 
-        <div> Project1</div>
-        <div> Project1</div>
-        <div> Project1</div>
-        <div> Project1</div>
-        <div> Project1</div>
+        <div className="relative group w-75 h-50 overflow-hidden rounded-lg">
+          <img
+            src="assets\Projects\Tinder.jpeg"
+            className="w-full h-full object-cover transition duration-300 group-hover:scale-110"
+          />
+          <div
+            className="absolute left-0 right-0 bottom-0  transform translate-y-full group-hover:translate-y-0  
+          flex items-center justify-center bg-gradient-to-t from-blue-500/50 to-white/25 h-full 
+            transition duration-300 text-white text-xl font-semibold"
+          >
+            Hover Text Here
+          </div>
+        </div>
+
+        <div className="relative group w-75 h-50 overflow-hidden rounded-lg">
+          <img
+            src="assets\Projects\Tinder.jpeg"
+            className="w-full h-full object-cover transition duration-300 group-hover:scale-110"
+          />
+          <div
+            className="absolute left-0 right-0 bottom-0  transform translate-y-full group-hover:translate-y-0  
+          flex items-center justify-center bg-gradient-to-t from-blue-500/50 to-white/25 h-full 
+            transition duration-300 text-white text-xl font-semibold"
+          >
+            Hover Text Here
+          </div>
+        </div>
+        <div className="relative group w-75 h-50 overflow-hidden rounded-lg">
+          <img
+            src="assets\Projects\Tinder.jpeg"
+            className="w-full h-full object-cover transition duration-300 group-hover:scale-110"
+          />
+          <div
+            className="absolute left-0 right-0 bottom-0  transform translate-y-full group-hover:translate-y-0  
+          flex items-center justify-center bg-gradient-to-t from-blue-500/50 to-white/25 h-full 
+            transition duration-300 text-white text-xl font-semibold"
+          >
+            Hover Text Here
+          </div>
+        </div>
+        <div className="relative group w-75 h-50 overflow-hidden rounded-lg">
+          <img
+            src="assets\Projects\Tinder.jpeg"
+            className="w-full h-full object-cover transition duration-300 group-hover:scale-110"
+          />
+          <div
+            className="absolute left-0 right-0 bottom-0  transform translate-y-full group-hover:translate-y-0  
+          flex items-center justify-center bg-gradient-to-t from-blue-500/50 to-white/25 h-full 
+            transition duration-300 text-white text-xl font-semibold"
+          >
+            Hover Text Here
+          </div>
+        </div>
+    
       </div>
     </>
   );
